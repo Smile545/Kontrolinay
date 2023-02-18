ae907891c3f32e5e796cfe66b9abc2d4847734f4
Andrey <an.nikishin2012@yandex.ru>
2023-02-18 13:53:52 +0300

    Написан цикл для заполнения  двух массивов

--- Program.cs
+++ Program.cs
@@ -6,3 +6,18 @@
     }
     Console.WriteLine();
 }
+
+Console.Write("Задайти длину массива ");
+int a = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[a];
+string[] newarray = new string[a];
+for (int i = 0; i < a; i++)
+{
+    Console.WriteLine("Введите текст для массива");
+    string c = Console.ReadLine();
+    array[i] = c;
+    if (array[i].Length <= 3)
+    {
+        newarray[i] = array[i];
+    }
+}
